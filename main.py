4693b82e95d64bfe940ca127c5744f48d82b2173
Areej-FA <46886699+Areej-FA@users.noreply.github.com>
2021-03-10 18:43:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# this is day 1 of 100 day of code in python
+
+cityName = input("please enter the city you grow up in? ")
+
+petName = input("please enter your pet name? ")
+
+print("your brand name is " + cityName + " " + petName)
